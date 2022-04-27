69e03bdbac6a18bf31a49d6ab5d408328a4ec1a2
davidcoats <david@getsafetycone.com>
2022-04-27 15:36:47 -0700

    Interim changes.

--- source/R5T.T0124.X0001/Code/Bases/Extensions/ISearchPatternExtensions.cs
+++ source/R5T.T0124.X0001/Code/Bases/Extensions/ISearchPatternExtensions.cs
@@ -9,6 +9,23 @@ namespace System
 {
     public static class ISearchPatternExtensions
     {
+        public static string AllCSharpProjectFiles(this ISearchPattern _)
+        {
+            var output = Instances.SearchPatternOperator.AllFilesWithExtension(
+                Instances.FileExtension.CSharpProjectFile());
+
+            return output;
+        }
+
+        /// <summary>
+        /// Chooses <see cref="AllCSharpProjectFiles(ISearchPattern)"/> as the default for projects.
+        /// </summary>
+        public static string AllProjectFiles(this ISearchPattern _)
+        {
+            var output = _.AllCSharpProjectFiles();
+            return output;
+        }
+
         public static string AllSolutionFiles(this ISearchPattern _)
         {
             var output = Instances.SearchPatternOperator.AllFilesWithExtension(
